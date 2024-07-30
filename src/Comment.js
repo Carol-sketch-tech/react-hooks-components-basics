f85f940eb333f40e7f4854d95e627363fac35a4d
Caroline <bancroftwanjiru10000@gmail.com>
2024-07-30 17:03:11 +0300

    compelete

--- src/Comment.js
+++ src/Comment.js
@@ -5,3 +5,12 @@ function Comment() {
 }
 
 export default Comment;
+// example of an arrow functional compnent syntax
+ const Comment =()=> <div>Naturally, I agree witht his article.</div>
+
+//  example of a class component looks like this :
+class Comment extends React.Component {
+  render(){
+    return<div>Naturally, I agree with this article.</div>
+  }
+}
